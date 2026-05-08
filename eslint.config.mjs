c74e92c8ef1e69fc39d012282fb7c689449d20c1
Kazi Ahmed <kaziniazahmed@gmail.com>
2026-05-08 11:57:22 -0400

    chore: configure prettier, eslint, and pre-commit hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextTypescript from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  ...nextTypescript,
+  prettier,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/consistent-type-imports": "warn",
+    },
+  },
+  globalIgnores([".next/**", "out/**", "build/**", "node_modules/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
